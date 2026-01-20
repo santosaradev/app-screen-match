c472e9c679217a639496f5bc462f4211f42a5d67
Sara Santos <sarasantosdev@outlook.com>
2026-01-20 16:58:00 -0300

    feat: utiliza entidade Movie na aplicação

---
+++ src/ScreenMatchApp.java
@@ -0,0 +1,11 @@
+public class ScreenMatchApp {
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+
+        movie.name = "The GodFather";
+        movie.year = 1972;
+        movie.duration = 177;
+        movie.included = true;
+        movie.rating = 8.1;
+    }
+}
