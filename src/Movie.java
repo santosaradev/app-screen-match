18b3a3d4761dd8dfb5a86033984ec80122af2d52
Sara Santos <sarasantosdev@outlook.com>
2026-01-20 16:54:32 -0300

    feat: adiciona entidade Movie ao domínio

---
+++ src/Movie.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String name;
+    int year;
+    int duration;
+    boolean included;
+    double rating;
+    List <Integer> ratings = new ArrayList<>();
+}
