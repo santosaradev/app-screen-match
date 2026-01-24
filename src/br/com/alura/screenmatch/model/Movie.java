3a8c1f39aa7758e82782241472e9d927a8b1a858
Sara Santos <sarasantosdev@outlook.com>
2026-01-24 17:17:51 -0300

    feat: adiciona o construtor da super classe

--- src/br/com/alura/screenmatch/model/Movie.java
+++ src/br/com/alura/screenmatch/model/Movie.java
@@ -1,48 +1,10 @@
 package br.com.alura.screenmatch.model;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Movie extends Title {
+    private String director;
 
-public class Movie {
-    private String name;
-    private int year;
-    private int duration;
-    private boolean included;
-    private List<Integer> ratings = new ArrayList<Integer>();
-
-    public Movie(String name, int year, int duration, boolean included){
-        this.name = name;
-        this.year = year;
-        this.duration = duration;
-        this.included = included;
+    public Movie(String name, int year, int duration, boolean included) {
+        super(name, year, duration, included);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public void setIncluded(boolean included) {
-        this.included = included;
-    }
-
-    public List<Integer> getRatings() {
-        return ratings;
-    }
-
-    public void showTechnicSlip(){
-        System.out.println("Nome: " + name);
-        System.out.println("Ano: " + year);
-    }
-
-    public double calculateMean(List <Integer> ratings){
-        return ratings.stream().mapToInt(Integer::intValue).average().orElse(0.0);
-    }
 }
