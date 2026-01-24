c01198208f7fa5616ed8392927b6b741d5fdb761
Sara Santos <sarasantosdev@outlook.com>
2026-01-23 21:18:18 -0300

    feat: adiciona modificador de acesso e métodos

--- src/Movie.java
+++ src/Movie.java
@@ -6,6 +6,25 @@ public class Movie {
     int year;
     int duration;
     boolean included;
-    double rating;
-    List <Integer> ratings = new ArrayList<>();
+    private List<Integer> ratings = new ArrayList<Integer>();
+
+    public Movie(String name, int year, int duration, boolean included){
+        this.name = name;
+        this.year = year;
+        this.duration = duration;
+        this.included = included;
+    }
+
+    public List<Integer> getRatings() {
+        return ratings;
+    }
+
+    public void showTechnicSlip(){
+        System.out.println("Nome: " + name);
+        System.out.println("Ano: " + year);
+    }
+
+    public double calculateMean(List <Integer> ratings){
+        return ratings.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+    }
 }
