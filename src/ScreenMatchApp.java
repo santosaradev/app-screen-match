2325359eb16d1f0a514b13b17252f203e00dd490
Sara Santos <sarasantosdev@outlook.com>
2026-01-25 21:28:28 -0300

    feat: cria objeto série

--- src/ScreenMatchApp.java
+++ src/ScreenMatchApp.java
@@ -1,8 +1,11 @@
 import br.com.alura.screenmatch.model.Movie;
+import br.com.alura.screenmatch.model.Serie;
 
 public class ScreenMatchApp {
     public static void main(String[] args) {
         Movie movie = new Movie("The GodFather", 1972, 177, true);
+
+        System.out.println("Ficha técnica do filme");
 
         movie.showTechnicSlip();
 
@@ -12,6 +15,17 @@ public class ScreenMatchApp {
 
         double meanResult = movie.calculateMean(movie.getRatings());
 
-        System.out.println("Média: " + meanResult);
+        System.out.println(String.format("Média: %.1f", meanResult) + "\n");
+
+        Serie serie = new Serie("Lost", 2004, 0,false );
+
+        System.out.println("Ficha técnica da série");
+
+        serie.setSeason(6);
+        serie.setEpisode(121);
+        serie.setMinutesEpisode(40);
+        serie.getDuration();
+
+        serie.showTechnicSlip();
     }
 }
