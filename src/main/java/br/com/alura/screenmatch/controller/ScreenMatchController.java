29f961affb058d5d1da0c01c57273b46f66fe947
Sara Santos <sarasantosdev@outlook.com>
2026-01-30 23:31:50 -0300

    feat: adiciona método para salvar os dados no arquivo json

--- src/main/java/br/com/alura/screenmatch/controller/ScreenMatchController.java
+++ src/main/java/br/com/alura/screenmatch/controller/ScreenMatchController.java
@@ -1,14 +1,28 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.model.TitleOmdb;
+import br.com.alura.screenmatch.model.repository.ScreenMatchRepository;
 import br.com.alura.screenmatch.service.ScreenMatchService;
 
 import java.io.IOException;
 
 public class ScreenMatchController {
-    private final ScreenMatchService service = new ScreenMatchService();
+    private final ScreenMatchService service;
+    private final ScreenMatchRepository repository;
 
-    public TitleOmdb getTitle(String title) throws IOException, InterruptedException {
-        return service.titleSearch(title);
+    public ScreenMatchController() {
+        this.service =  new ScreenMatchService();
+        this.repository = new ScreenMatchRepository();
+    }
+
+    public void saveData(String title){
+        try{
+            TitleOmdb titleOmdb = service.titleSearch(title);
+            repository.save(titleOmdb);
+
+            System.out.println("Operação realizada com sucesso!");
+        }catch (Exception e){
+            System.out.println("Erro no processo: " + e.getMessage());
+        }
     }
 }
